ed1e6300f6e4643ca74305de38487a0f7ac12303
Maziar <sojodyan@gmail.com>
2022-06-01 02:11:58 -0400

    add sub route

--- main.go
+++ main.go
@@ -7,10 +7,17 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Home</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Index</h1>")
+}
+
+func maz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Maz</h1>")
 }
 
 func main() {
+	http.HandleFunc("/maz", maz)
 	http.HandleFunc("/", handlerFunc)
 	fmt.Println("starting the server on :3000")
 	err := http.ListenAndServe(":3000", nil)
