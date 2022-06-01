e1ba40df39270d45915e3af518f192fb141add66
Maziar <sojodyan@gmail.com>
2022-06-01 02:17:27 -0400

    use URL PATH

--- main.go
+++ main.go
@@ -16,9 +16,18 @@ func maz(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Maz</h1>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		handlerFunc(w, r)
+	} else if r.URL.Path == "/maz" {
+		maz(w, r)
+	}
+	fmt.Fprintf(w, r.URL.Path)
+}
+
 func main() {
-	http.HandleFunc("/maz", maz)
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/maz", pathHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("starting the server on :3000")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
