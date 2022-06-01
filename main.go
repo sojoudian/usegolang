9b859ca57be8c21d6454a6c1ef50195abc41be79
Maziar <sojodyan@gmail.com>
2022-06-01 16:30:07 -0400

    add router

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -13,24 +12,52 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 func maz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>Maz</h1>")
+	fmt.Fprintf(w, "<h1>Hello from Maz</h1>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
+//
+//func pathHandler(w http.ResponseWriter, r *http.Request) {
+//	//w.Header().Set("Content-Type", "text/html; charset=utf-8")
+//	switch r.URL.Path {
+//	case "/":
+//		handlerFunc(w, r)
+//	case "/maz":
+//		maz(w, r)
+//	default:
+//		http.Error(w, "Page note found", http.StatusNotFound)
+//		// this is equal to below two lines of code
+//		//w.WriteHeader(http.StatusNotFound)
+//		//fmt.Fprintf(w, "Page not found")
+//	}
+//	//fmt.Fprintln(w, r.URL.RawPath)
+//	//fmt.Fprintln(w, r.URL.Path)
+//}
+//
+
+type Router struct{}
+
+func (router Router) serveHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
 		handlerFunc(w, r)
-	} else if r.URL.Path == "/maz" {
+	case "/maz":
 		maz(w, r)
+	default:
+		http.Error(w, "Page note found", http.StatusNotFound)
 	}
-	fmt.Fprintf(w, r.URL.Path)
 }
 
 func main() {
-	http.HandleFunc("/maz", pathHandler)
-	http.HandleFunc("/", pathHandler)
-	fmt.Println("starting the server on :3000")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Panicln(err)
-	}
+	var router Router
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", router)
 }
+
+//func main() {
+//	var router Router
+//	//http.HandleFunc("/maz", pathHandler)
+//	//http.HandleFunc("/", pathHandler)
+//	fmt.Println("starting the server on :3000")
+//	http.ListenAndServe(":3000", router)
+//
+//}
